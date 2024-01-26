9661772aa346bfad8245f7aa7f73fd16f61636c1
hellory4n <hellory4n@gmail.com>
2024-01-26 12:52:02 -0500

    basic repl

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome to the C# Teapot REPL\nWrite `.exit` to exit\n");
+
+while (true) {
+    Console.Write("> ");
+    string input = Console.ReadLine();
+    if (input == ".exit") {
+        break;
+    }
+
+    Console.WriteLine("yep, that's a value lmao");
+}
