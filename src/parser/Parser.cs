9261f3490bfa008bf93f894d8e8ac1e373a18b4e
hellory4n <hellory4n@gmail.com>
2024-01-31 22:49:03 -0500

    shunting yard algorithm

--- src/parser/Parser.cs
+++ src/parser/Parser.cs
@@ -2,16 +2,19 @@ using csteapot.lexer;
 
 namespace csteapot.parser;
 
+// this parser uses the shunting yard algorithm to parse expressions, more info at https://en.wikipedia.org/wiki/Shunting_yard_algorithm
 static partial class Parser
 {
     public static List<IAstNode> Nodes = [];
     static int i = 0;
     static List<Token> tokens = [];
+    static Stack<Token> operators = [];
+    static Stack<Token> output = [];
 
     public static List<IAstNode> Parse(List<Token> tokens_)
     {
         // we add this so we don't have to deal with checking the end of the thing all the time
-        // we need .ToList() to copy the list
+        // c# doesn't have explicit reference stuff so we need .ToList() to copy the list
         tokens = tokens_.ToList();
         tokens.Add(new Token { Type = TokenType.End });
         tokens.Add(new Token { Type = TokenType.End });
@@ -22,11 +25,42 @@ static partial class Parser
             switch (t.Type) {
                 case TokenType.Integer:
                 case TokenType.Float:
-                    Nodes.Add(ParseExpression());
+                    output.Push(t);
+                    break;
+                
+                case TokenType.LParen:
+                    operators.Push(t);
+                    break;
+                
+                case TokenType.RParen:
+                    while (operators.Count > 0 && operators.Peek().Type != TokenType.LParen) {
+                        output.Push(operators.Pop());
+                    }
+                    operators.Pop(); // remove the (
+                    break;
+                
+                case TokenType.Plus:
+                case TokenType.Minus:
+                case TokenType.Star:
+                case TokenType.Slash:
+                case TokenType.Percent:
+                    while (operators.Count > 0 && GetPrecedence(operators.Peek().Type) >= GetPrecedence(t.Type)) {
+                        output.Push(operators.Pop());
+                    }
+                    operators.Push(t);
                     break;
             }
 
             i++;
+        }
+
+        Console.WriteLine("PARSING TIME\noperator stack");
+        foreach (var item in operators) {
+            Console.WriteLine(item);
+        }
+        Console.WriteLine("output stack");
+        foreach (var item in output) {
+            Console.WriteLine(item);
         }
 
         return Nodes;
--- src/parser/ParserUtils.cs
+++ src/parser/ParserUtils.cs
@@ -4,66 +4,11 @@ namespace csteapot.parser;
 
 static partial class Parser
 {
-    static IAstExpression ParseExpression()
+    static int GetPrecedence(TokenType tokenType)
     {
-        IAstExpression left = ParseTerm();
-
-        if (tokens[i].Type == TokenType.Plus || tokens[i].Type == TokenType.Minus) {
-            TokenType op = tokens[i].Type;
-            Continue();
-            IAstExpression right = ParseTerm();
-
-            return new AstBinaryExpression {
-                Left = left,
-                Operator = op,
-                Right = right
-            };
-        }
-
-        return left;
-    }
-
-    static IAstExpression ParseTerm()
-    {
-        IAstExpression left = ParseFactor();
-
-        // if only there was a language that let me express this more concisely
-        if (tokens[i].Type == TokenType.Star || tokens[i].Type == TokenType.Slash || tokens[i].Type == TokenType.Percent) {
-            TokenType op = tokens[i].Type;
-            Continue();
-            IAstExpression right = ParseFactor();
-
-            return new AstBinaryExpression {
-                Left = left,
-                Operator = op,
-                Right = right
-            };
-        }
-
-        return left;
-    }
-
-    /// <summary>
-    /// You might be wondering what a "term" or "factor" is, and those are brilliant questions.
-    /// </summary>
-    static IAstExpression ParseFactor()
-    {
-        if (tokens[i].Type == TokenType.Integer || tokens[i].Type == TokenType.Float) {
-            AstLiteral gjjdf = new() { Value = tokens[i].Literal };
-            Continue();
-            return gjjdf;
-        }
-        else if (tokens[i].Type == TokenType.LParen) {
-            Continue(); // consume the (
-            IAstExpression expression = ParseExpression();
-            Continue(); // consume the )
-            return expression;
-        }
-        else {
-            Console.WriteLine("Fatal error: What the fuck?");
-            Continue();
-            return default;
-        }
+        if (tokenType == TokenType.Plus || tokenType == TokenType.Minus) return 1;
+        if (tokenType == TokenType.Star || tokenType == TokenType.Slash || tokenType == TokenType.Percent) return 2;
+        return 0;
     }
 
     /// <summary>
