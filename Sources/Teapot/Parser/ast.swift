c92a8a61821250847211b760411f178d7ec9d8a5
hellory4n <hellory4n@gmail.com>
2024-04-03 21:22:37 -0500

    ast stuff

---
+++ Sources/Teapot/Parser/ast.swift
@@ -0,0 +1,44 @@
+import Foundation
+
+protocol Ast {}
+protocol Expression: Ast {}
+
+indirect enum ArithmeticExpr: Expression {
+    // 9
+    case literal(n: Double)
+    // -9
+    case negate(n: ArithmeticExpr)
+
+    // 9 + 10
+    case add(a: ArithmeticExpr, b: ArithmeticExpr)
+    // 9 - 10
+    case sub(a: ArithmeticExpr, b: ArithmeticExpr)
+    // 9 * 10
+    case mul(a: ArithmeticExpr, b: ArithmeticExpr)
+    // 9 / 10
+    case div(a: ArithmeticExpr, b: ArithmeticExpr)
+    // 9 % 10
+    case mod(a: ArithmeticExpr, b: ArithmeticExpr)
+}
+
+extension ArithmeticExpr: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .literal(let n):
+            return "(literal \(n))"
+        case .negate(let n):
+            return "(negate \(n))"
+        
+        case .add(a: let a, b: let b):
+            return "(\(a) + \(b))"
+        case .sub(a: let a, b: let b):
+            return "(\(a) - \(b))"
+        case .mul(a: let a, b: let b):
+            return "(\(a) * \(b))"
+        case .div(a: let a, b: let b):
+            return "(\(a) / \(b))"
+        case .mod(a: let a, b: let b):
+            return "(\(a) % \(b))"
+        }
+    }
+}
